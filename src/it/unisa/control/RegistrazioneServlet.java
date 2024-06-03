99a4afc1f8fef99d3c122287062acb56a841ef9a
GiovanniLav <g.lavorato@studenti.unisa.it>
2024-06-03 17:54:11 +0200

    ID-6 resolved

--- src/it/unisa/control/RegistrazioneServlet.java
+++ src/it/unisa/control/RegistrazioneServlet.java
@@ -1,6 +1,7 @@
 package it.unisa.control;
 
 import java.io.IOException;
+
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -12,7 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import it.unisa.model.*;
-
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Date;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RegistraziomeServlet
  */
@@ -27,39 +36,55 @@ public class RegistrazioneServlet extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		UserDao dao = new UserDao();
-		String nome = request.getParameter("nome");
-		String cognome = request.getParameter("cognome");
-		String email = request.getParameter("email");
-		String dataNascita = request.getParameter("nascita");
-		String username = request.getParameter("us");
-		String pwd = request.getParameter("pw");
+        UserDao dao = new UserDao();
+        String nome = request.getParameter("nome");
+        String cognome = request.getParameter("cognome");
+        String email = request.getParameter("email");
+        String dataNascita = request.getParameter("nascita");
+        String username = request.getParameter("us");
+        String pwd = request.getParameter("pw");
 
         String[] parti = dataNascita.split("-");
         dataNascita = parti[2] + "-" + parti[1] + "-" + parti[0];
-		
-		try {
-			
-			UserBean user = new UserBean();
-			user.setNome(nome);
-			user.setCognome(cognome);
-			user.setEmail(email);
-			user.setDataDiNascita(Date.valueOf(dataNascita));
-			user.setUsername(username);
-			user.setPassword(pwd);
-			user.setAmministratore(false);
-			user.setCap(null);
-			user.setIndirizzo(null);
-			user.setCartaDiCredito(null);
-			dao.doSave(user);
-			
-		}catch(SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-				
-		response.sendRedirect(request.getContextPath() + "/Home.jsp");
 
-	}
+        try {
+            // Hash della password utilizzando SHA-512
+            String hashedPwd = hashPassword(pwd);
+
+            UserBean user = new UserBean();
+            user.setNome(nome);
+            user.setCognome(cognome);
+            user.setEmail(email);
+            user.setDataDiNascita(Date.valueOf(dataNascita));
+            user.setUsername(username);
+            user.setPassword(hashedPwd); // Salva la password hashata nel bean
+            user.setAmministratore(false);
+            user.setCap(null);
+            user.setIndirizzo(null);
+            user.setCartaDiCredito(null);
+            dao.doSave(user);
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+
+        response.sendRedirect(request.getContextPath() + "/Home.jsp");
+    }
+
+    // Metodo per hashare la password utilizzando SHA-512
+    private String hashPassword(String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+            byte[] hashedBytes = messageDigest.digest(password.getBytes());
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : hashedBytes) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+            return stringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
+
+
